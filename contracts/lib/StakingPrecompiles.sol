0272d6506be6ca3b28ea4453d7f2f3a8f686dd46
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2021-10-26 01:54:42 +0530

    Use abi.encodeWithSelector instead of directive
    This will save the assembly call 28 bytes of storage, since a selector
    occupies 4 bytes, while a padded directive uint8 takes 32 bytes

--- contracts/lib/StakingPrecompiles.sol
+++ contracts/lib/StakingPrecompiles.sol
@@ -1,13 +1,21 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract StakingPrecompiles {
-    //0 => createValidator
-    //1 => editValidator
-    //2 => delegate
-    //3 => unDelegate
-    //4 => collectRewards
-    enum Directive{
+struct Description {
+    string name;
+    string identity;
+    string website;
+    string securityContact;
+    string details;
+}
+
+struct CommissionRate {
+    string rate;
+    string maxRate;
+    string maxChangeRate;
+}
+
+enum Directive{
         CREATE_VALIDATOR,
         EDIT_VALIDATOR,
         DELEGATE,
@@ -15,21 +23,35 @@ contract StakingPrecompiles {
         COLLECT_REWARDS
     }
 
-    struct Description {
-        string name;
-        string identity;
-        string website;
-        string securityContact;
-        string details;
-    }
+abstract contract StakingPrecompilesSelectors {
+  function CreateValidator( address validatorAddress,
+                            Description memory description,
+                            CommissionRate memory commissionRates,
+                            uint256 minSelfDelegation,
+                            uint256 maxTotalDelegation,
+                            bytes[] memory slotPubKeys,
+                            bytes[] memory slotKeySigs,
+                            uint256 amount) public virtual;
+  function EditValidator(address validatorAddress,
+                        Description memory description,
+                        string memory commissionRate,
+                        uint256 minSelfDelegation,
+                        uint256 maxTotalDelegation,
+                        bytes memory slotKeyToRemove,
+                        bytes memory slotKeyToAdd,
+                        bytes memory slotKeyToAddSig) public virtual;
+  function Delegate(address delegatorAddress,
+                    address validatorAddress,
+                    uint256 amount) public virtual;
+  function Undelegate(address delegatorAddress,
+                      address validatorAddress,
+                      uint256 amount) public virtual;
+  function CollectRewards(address delegatorAddress) public virtual;
+}
 
-    struct CommissionRate {
-        string rate;
-        string maxRate;
-        string maxChangeRate;
-    }
-
-    function createValidator(Description memory description,
+contract StakingPrecompiles {
+  
+    function createValidator( Description memory description,
                               CommissionRate memory commission,
                               uint256 minSelfDelegation,
                               uint256 maxTotalDelegation,
@@ -37,7 +59,7 @@ contract StakingPrecompiles {
                               bytes[] memory slotKeySigs,
                               uint256 amount)
                             public returns (uint256 result) {
-        bytes memory encodedInput = abi.encode(uint8(Directive.CREATE_VALIDATOR),
+        bytes memory encodedInput = abi.encodeWithSelector(StakingPrecompilesSelectors.CreateValidator.selector,
                                         address(this),
                                         description,
                                         commission,
@@ -47,6 +69,7 @@ contract StakingPrecompiles {
                                         slotKeySigs,
                                         amount);
         bytes32 sizeOfInput;
+
         assembly {
             let memPtr := mload(0x40)
             sizeOfInput := add(mload(encodedInput), 32)
@@ -54,17 +77,25 @@ contract StakingPrecompiles {
         }
     }
 
-    function editValidator(Description memory description,
-                          string memory commissionRate,
-                          uint256 minSelfDelegation,
-                          uint256 maxTotalDelegation,
-                          bytes memory slotKeyToRemove,
-                          bytes memory slotKeyToAdd,
-                          bytes memory slotKeyToAddSig)
-                        public returns (uint256 result) {
-        bytes memory encodedInput = abi.encode(uint8(Directive.EDIT_VALIDATOR), address(this), description, commissionRate, minSelfDelegation, maxTotalDelegation, slotKeyToRemove, slotKeyToAdd, slotKeyToAddSig);
-
+    function editValidator( Description memory description,
+                            string memory commissionRate,
+                            uint256 minSelfDelegation,
+                            uint256 maxTotalDelegation,
+                            bytes memory slotKeyToRemove,
+                            bytes memory slotKeyToAdd,
+                            bytes memory slotKeyToAddSig)
+                            public returns (uint256 result) {
+        bytes memory encodedInput = abi.encodeWithSelector(StakingPrecompilesSelectors.EditValidator.selector,
+                                        address(this),
+                                        description,
+                                        commissionRate,
+                                        minSelfDelegation,
+                                        maxTotalDelegation,
+                                        slotKeyToRemove,
+                                        slotKeyToAdd,
+                                        slotKeyToAddSig);
         bytes32 sizeOfInput;
+
         assembly {
             let memPtr := mload(0x40)
             sizeOfInput := add(mload(encodedInput), 32)
@@ -73,10 +104,12 @@ contract StakingPrecompiles {
     }
 
     function delegate(address validatorAddress, uint256 amount) public returns (uint256 result) {
-        bytes memory encodedInput = abi.encode(uint8(Directive.DELEGATE), address(this), validatorAddress, amount);
-        //bytes memory encodedInput = abi.encodePacked(uint8(Directive.DELEGATE), encodedInput1);
-
+        bytes memory encodedInput = abi.encodeWithSelector(StakingPrecompilesSelectors.Delegate.selector,
+                                        address(this),
+                                        validatorAddress,
+                                        amount);
         bytes32 sizeOfInput;
+
         assembly {
             let memPtr := mload(0x40)
             sizeOfInput := add(mload(encodedInput), 32)
@@ -85,25 +118,29 @@ contract StakingPrecompiles {
     }
 
     function undelegate(address validatorAddress, uint256 amount) public returns (uint256 result) {
-        bytes memory encodedInput = abi.encode(uint8(Directive.UNDELEGATE), address(this), validatorAddress, amount);
+      bytes memory encodedInput = abi.encodeWithSelector(StakingPrecompilesSelectors.Undelegate.selector,
+                                      address(this),
+                                      validatorAddress,
+                                      amount);
+      bytes32 sizeOfInput;
 
-        bytes32 sizeOfInput;
-        assembly {
-            let memPtr := mload(0x40)
-            sizeOfInput := add(mload(encodedInput), 32)
-            result := call(gas(), 0xfc, 0x0, encodedInput, sizeOfInput, memPtr, 0x20)
-        }
+      assembly {
+          let memPtr := mload(0x40)
+          sizeOfInput := add(mload(encodedInput), 32)
+          result := call(gas(), 0xfc, 0x0, encodedInput, sizeOfInput, memPtr, 0x20)
+      }
     }
 
     function collectRewards() public returns (uint256 result) {
-        bytes memory encodedInput = abi.encode(uint8(Directive.COLLECT_REWARDS), address(this));
+      bytes memory encodedInput = abi.encodeWithSelector(StakingPrecompilesSelectors.CollectRewards.selector,
+                                      address(this));
+      bytes32 sizeOfInput;
 
-        bytes32 sizeOfInput;
-        assembly {
-            let memPtr := mload(0x40)
-            sizeOfInput := add(mload(encodedInput), 32)
-            result := call(gas(), 0xfc, 0x0, encodedInput, sizeOfInput, memPtr, 0x20)
-        }
+      assembly {
+          let memPtr := mload(0x40)
+          sizeOfInput := add(mload(encodedInput), 32)
+          result := call(gas(), 0xfc, 0x0, encodedInput, sizeOfInput, memPtr, 0x20)
+      }
     }
 
 }
--- contracts/mock/MaliciousContract.sol
+++ contracts/mock/MaliciousContract.sol
@@ -1,27 +1,22 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract MaliciousContract {
-    //0 => createValidator
-    //1 => editValidator
-    //2 => delegate
-    //3 => unDelegate
-    //4 => collectRewards
-    enum Directive{
-        CREATE_VALIDATOR,
-        EDIT_VALIDATOR,
-        DELEGATE,
-        UNDELEGATE,
-        COLLECT_REWARDS
-    }
+import { StakingPrecompilesSelectors, Directive } from "../lib/StakingPrecompiles.sol";
 
-    function collectRewards(address victim_address) public returns (uint256 result) {
-        bytes memory encodedInput = abi.encode(uint8(Directive.COLLECT_REWARDS), victim_address);
-        bytes32 sizeOfInput;
-        assembly {
-            let memPtr := mload(0x40)
-            sizeOfInput := add(mload(encodedInput), 32)
-            result := call(gas(), 0xfc, 0x0, encodedInput, sizeOfInput, memPtr, 0x20)
-        }
+contract MaliciousContract {
+  event StakingPrecompileCalled(uint8 directive, bool success);
+
+  function collectRewards(address victim_address) public returns (uint256 result) {
+    bytes memory encodedInput = abi.encodeWithSelector(StakingPrecompilesSelectors.CollectRewards.selector,
+                                    victim_address);
+
+    bytes32 sizeOfInput;
+    assembly {
+        let memPtr := mload(0x40)
+        sizeOfInput := add(mload(encodedInput), 32)
+        result := call(gas(), 0xfc, 0x0, encodedInput, sizeOfInput, memPtr, 0x20)
     }
+    bool success = result != 0;
+    emit StakingPrecompileCalled(uint8(Directive.CREATE_VALIDATOR), success);
+  }
 }
