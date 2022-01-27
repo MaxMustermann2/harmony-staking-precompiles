fbe47e863d0d7763046133eec3c47d0509df6bd6
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2022-01-27 01:04:38 +0000

    Update comments, remove dangling comma

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { ContractFactory } = require( '@harmony-js/contract' );
 const { Wallet } = require( '@harmony-js/account' );
 const { Messenger, HttpProvider } = require( '@harmony-js/network' );
 const { ChainID, ChainType, hexToNumber } = require( '@harmony-js/utils' );
-const { toWei, toGwei, Units } = require('@harmony-js/utils');
+const { toWei, Units } = require('@harmony-js/utils');
 // read endpoint from .env
 require( 'dotenv' ).config();
 const endpoint = process.env.endpoint;
@@ -25,9 +25,11 @@ contract.methods.Delegate(
   '0xA5241513DA9F4463F1d4874b548dFBAC29D91f34', // delegator address
   '0xA5241513DA9F4463F1d4874b548dFBAC29D91f34', // validator address
   toWei( 100, Units.one ),                      // amount
-  // gasPrice of 30 gwei
-).send( { gasPrice: '0x6FC23AC00', gasLimit: 46000 } ).then( response => {
+).send(
+    {
+      gasPrice: toWei( 30, Units.Gwei ),        // network gas limit
+      gasLimit: 46000                           // ~23k for base + 23k for staking tx
+    }
+).then( response => {
   console.log( response );
-  // console.log( response.transaction.txStatus );
-  // console.log( response.transaction.receipt );
 } );
